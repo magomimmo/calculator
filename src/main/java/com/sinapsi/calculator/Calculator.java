d885fb353d720584c0376a6373bd4fb1b6276126
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2018-04-13 17:21:28 +0200

    Add javadoc to pass checktype rule

--- src/main/java/com/sinapsi/calculator/Calculator.java
+++ src/main/java/com/sinapsi/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.sinapsi.calculator;
 import org.springframework.stereotype.Service;
-
+/**
+* This program implements a simple calculator
+*/
 @Service
 public class Calculator {
     int sum(int a, int b) {
